8e67a6fc933958d4c4ac7907aabde076616c71df
Kamrul Hasan <kamrul@asabd.org>
2022-10-20 13:46:51 +0600

    done.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,19 @@ import Posts from "../../components/posts/Posts"
 import SlideBar from "../../components/sidebar/SideBar"
 import "./home.css"
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [posts,setPosts] = useState([]);
+  useEffect(() =>{
+    const fetchPosts = async() => {
+      const res = await axios.get("/member/GetMembers")
+      console.log(res);
+    }
+    fetchPosts()
+  },[])
   return (
     <>
         <Header />
